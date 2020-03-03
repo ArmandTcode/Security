6999e0da4b936a0d8948c420aa49a771fda8322a
Armand-Tiberiu Ersek <armand.ersek@fortech.ro>
2020-03-03 13:48:03 +0200

    Initial commit

---
+++ src/test/java/tb/spring/Templar/TemplarApplicationTests.java
@@ -0,0 +1,13 @@
+package tb.spring.Templar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
