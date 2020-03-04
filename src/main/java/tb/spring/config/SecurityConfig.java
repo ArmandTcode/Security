770e89e1a5f5f62b0c6ac7aeac075402e21dd2f5
Armand-Tiberiu Ersek <armand.ersek@fortech.ro>
2020-03-04 15:01:46 +0200

    Basic example for authorization

--- src/main/java/tb/spring/config/SecurityConfig.java
+++ src/main/java/tb/spring/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package tb.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,11 +15,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user")
                 .password("pass")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("add")
+                .roles("ADMIN");
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    protected PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
